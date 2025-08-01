924f74c78fc0d1b7680b5aec2740ece8f851a572
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-01 22:26:04 +0200

    implement ProjectsController then TasksController  with all CRUD action

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @projects = current_user.projects
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render :new
+    end
+  end
+
+  def new
+    @project = current_user.projects.new
+  end
+
+  def edit
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def show
+    @project = current_user.projects.find(params[:id])
+    @tasks = @project.tasks
+  end
+
+  def update
+    @project = current_user.projects.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = current_user.projects.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get "tasks/index"
+  get "tasks/show"
+  get "tasks/new"
+  get "tasks/create"
+  get "tasks/update"
+  get "tasks/destroy"
+  get "projects/index"
+  get "projects/create"
+  get "projects/new"
+  get "projects/edit"
+  get "projects/show"
+  get "projects/update"
+  get "projects/destroy"
   devise_for :users
   root to: "projects#index"
 
