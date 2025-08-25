87b235a2a1a6f4097e24ffdc9f1eb1658533627c
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-25 16:12:06 +0200

    fix: toast localisation

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,10 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+// Import and register all your controllers from the importmap under controllers/*
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
 
 export { application }
