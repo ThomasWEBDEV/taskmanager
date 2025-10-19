5283ca1ee867e6adbefa5a7cbff20858c9b55077
Feret Thomas <thomas.feret@hotmail.fr>
2025-10-19 15:50:17 +0200

    Fix: Added missing root route to config/routes.rb and confirmed fix for 'toggle_complete_task_path' in projects view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root "projects#index" # <--- AJOUT CRITIQUE
+
   resources :projects do
     member { patch :complete_all_tasks }
     resources :tasks, only: [:new, :create]
@@ -9,11 +11,7 @@ Rails.application.routes.draw do
   resources :tasks do
     member do
       patch :reorder
-      # Nous ajoutons 'as: :task_toggle_complete' pour garantir le nom du helper path
-      patch :toggle_complete, as: :toggle_complete
+      patch :toggle_complete
     end
   end
-
-  # Ajoutez la route racine ici si elle n'y est pas
-  # root "projects#index"
 end
